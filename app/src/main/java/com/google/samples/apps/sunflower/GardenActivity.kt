9ba964a62bb96c14bb1e58d94231959918f9f09e
vihaan <vihaanverama24@gmail.com>
2019-05-26 14:07:46 +0530

    deps added

--- app/src/main/java/com/google/samples/apps/sunflower/GardenActivity.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/GardenActivity.kt
@@ -1,12 +1,12 @@
 package com.google.samples.apps.sunflower
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class GardenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_garden)
     }
 }
